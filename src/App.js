f313033219ae271fe7aafb6624313bbb959031c7
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2020-02-09 19:49:42 +0500

    update shop page, collection preview comp and collectionitem component

--- src/App.js
+++ src/App.js
@@ -3,29 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => {
-	return (
-		<div>
-			<h1>HATS PAGE</h1>
-		</div>
-	);
-};
-
-// function App() {
-// 	return (
-// 		<div>
-// 			<HomePage />
-// 		</div>
-// 	);
-// }
+import ShopPage from './pages/shop/shopPage.component';
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route path='/hats' component={HatsPage} />
+				<Route path='/shop' component={ShopPage} />
 			</Switch>
 		</div>
 	);
