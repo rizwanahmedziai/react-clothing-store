7b7424e6fc4cc38a5903c9063be884cafb3f3184
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2020-02-20 14:18:43 +0500

    Completed Google sign in option with button styling

--- src/App.js
+++ src/App.js
@@ -7,17 +7,42 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shopPage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
 
-function App() {
-	return (
-		<div>
-			<Header />
-			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route path='/shop' component={ShopPage} />
-				<Route path='/signin' component={SignInAndSignUpPage} />
-			</Switch>
-		</div>
-	);
+import { auth } from './firebase/firebase.utils';
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser: null
+		};
+	}
+
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({ currentUser: user });
+			console.log(user);
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div>
+				<Header currentUser={this.state.currentUser} />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
+					<Route path='/signin' component={SignInAndSignUpPage} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
