9ceaf74628efbd895de9df6593883d8870663b17
Rizwan Ahmed <rizwanahmedziai@gmail.com>
2020-02-20 13:09:46 +0500

    Implemented Signin with Google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,8 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../cutom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends React.Component {
 	constructor(props) {
 		super(props);
@@ -49,6 +51,9 @@ class SignIn extends React.Component {
 					/>
 
 					<CustomButton type='submit'>Sign In</CustomButton>
+					<CustomButton onClick={signInWithGoogle}>
+						Sign in with Google
+					</CustomButton>
 				</form>
 			</div>
 		);
